12107720c43843eb2c9918ba523efbb5de6b4c45
Quirianyailet Perez <qperez1@usf.edu>
2024-09-01 14:49:52 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+// Task 1: Variables and Data Types
+let employeeName = "Candy Man";
+const employeeID =33;
+var isactive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof (employeeID));
+console.log(isactive, typeof (isactive));
+
